02cb6ed57e6250a00f1739e1145bb2df2f01c246
Fabricio Bezerra <demonstro003@gmail.com>
2017-11-21 16:45:08 -0200

    Visitor can see recipes on homepage
    * Fixed typos

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :recipe do
+    title 'Bolo de cenoura'
+    recipe_type 'Sobremesa'
+    cuisine 'Brasileira'
+    difficulty 'Médio'
+    cook_time 50
+  end
+end
--- spec/features/visitor_visits_homepage_spec.rb
+++ spec/features/visitor_visits_homepage_spec.rb
@@ -11,4 +11,58 @@ feature 'Visitor visits homepage' do
       end
     end
   end
+
+  describe 'and sees recipes' do
+    scenario 'sucessfully' do
+      banana_assada = create(
+        :recipe, title: 'Banana assada', recipe_type: 'Sobremesa',
+                 cuisine: 'Brasileira', difficulty: 'Fácil', cook_time: 60
+      )
+
+      bolo_laranja = create(
+        :recipe, title: 'Bolo de laranja', recipe_type: 'Sobremesa',
+                 cuisine: 'Brasileira', difficulty: 'Médio', cook_time: 60
+      )
+
+      visit root_path
+
+      within "div.card#recipe_#{banana_assada.id}" do
+        within 'div.card-body' do
+          expect(page).to have_css('h4.card-title', text: banana_assada.title)
+          expect(page).to have_css('dl')
+          expect(page).to have_css('dt', text: 'Tipo de receita')
+          expect(page).to have_css('dd', text: banana_assada.recipe_type)
+          expect(page).to have_css('dt', text: 'Cozinha')
+          expect(page).to have_css('dd', text: banana_assada.cuisine)
+          expect(page).to have_css('dt', text: 'Dificuldade')
+          expect(page).to have_css('dd', text: banana_assada.difficulty)
+          expect(page).to have_css('dt', text: 'Tempo de preparo')
+          expect(page).to have_css('dd', text: "#{banana_assada.cook_time} \
+minutos")
+        end
+      end
+
+      within "div.card#recipe_#{bolo_laranja.id}" do
+        within 'div.card-body' do
+          expect(page).to have_css('h4.card-title', text: bolo_laranja.title)
+          expect(page).to have_css('dl')
+          expect(page).to have_css('dt', text: 'Tipo de receita')
+          expect(page).to have_css('dd', text: bolo_laranja.recipe_type)
+          expect(page).to have_css('dt', text: 'Cozinha')
+          expect(page).to have_css('dd', text: bolo_laranja.cuisine)
+          expect(page).to have_css('dt', text: 'Dificuldade')
+          expect(page).to have_css('dd', text: bolo_laranja.difficulty)
+          expect(page).to have_css('dt', text: 'Tempo de preparo')
+          expect(page).to have_css('dd', text: "#{bolo_laranja.cook_time} \
+minutos")
+        end
+      end
+    end
+
+    scenario 'and do not see any recipe' do
+      visit root_path
+
+      expect(page).to have_content('Nenhuma receita encontrada')
+    end
+  end
 end
