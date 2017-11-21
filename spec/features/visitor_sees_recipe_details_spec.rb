e190bb1f89f4c157dadef0ab3c81350e0e7bd09a
Fabricio Bezerra <demonstro003@gmail.com>
2017-11-21 17:23:36 -0200

    Visitor sees recipe details
    * And can return to homepage

---
+++ spec/features/visitor_sees_recipe_details_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+feature 'visitor visits recipe' do
+  scenario 'sucessfully' do
+    banana_assada = create(
+      :recipe, title: 'Banana assada', recipe_type: 'Sobremesa',
+               cuisine: 'Brasileira', difficulty: 'Fácil', cook_time: 60,
+               ingredients: 'Banana', method: 'Asse a banana por 60 minutos'
+    )
+
+    visit root_path
+    click_on banana_assada.title
+
+    expect(page).to have_css('h1', banana_assada.title)
+    expect(page).to have_css('dl')
+    expect(page).to have_css('dt', text: 'Tipo de receita')
+    expect(page).to have_css('dd', text: banana_assada.recipe_type)
+    expect(page).to have_css('dt', text: 'Cozinha')
+    expect(page).to have_css('dd', text: banana_assada.cuisine)
+    expect(page).to have_css('dt', text: 'Dificuldade')
+    expect(page).to have_css('dd', text: banana_assada.difficulty)
+    expect(page).to have_css('dt', text: 'Tempo de preparo')
+    expect(page).to have_css('dd', text: "#{banana_assada.cook_time} minutos")
+    expect(page).to have_css('dt', text: 'Ingredientes')
+    expect(page).to have_css('dd', text: banana_assada.ingredients)
+    expect(page).to have_css('h2', text: 'Como preparar')
+    expect(page).to have_css('p', text: banana_assada.method)
+  end
+
+  scenario 'and returns to homepage' do
+    banana_assada = create(
+      :recipe, title: 'Banana assada', recipe_type: 'Sobremesa',
+               cuisine: 'Brasileira', difficulty: 'Fácil', cook_time: 60,
+               ingredients: 'Banana', method: 'Asse a banana por 60 minutos'
+    )
+
+    visit root_path
+    click_on banana_assada.title
+    click_on 'Voltar para a página inicial'
+
+    expect(current_path).to eq(root_path)
+  end
+end
